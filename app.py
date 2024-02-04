624c424eb8040347c02942ba95893cfa0042913d
Noah <106123749+nbran3@users.noreply.github.com>
2024-02-04 13:28:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+### Import libaries
 import folium
 import os
 import pandas as pd
 import json
 import pypyodbc
 
+### Create a variable for folium to read the JSON file. This might not be needed if you are working with countries for example, but since I was using counties I got a shapefile (json). 
 counties = os.path.join('data', 'counties.geojson')
 
-
+### You can either connect to a local SQL database or just use a CSV for the data. I would recommeded using a CSV file unless the data you are using is extremely big, but I did this for experimental reasons. 
 conn = pypyodbc.connect('Driver={SQL SERVER};'
                         'Server=DESKTOP-K4HURDS\SQLEXPRESS01;'
                         'Database=Projects;'
@@ -20,7 +22,28 @@ WHERE GDP2022 < 200000
 rpiResults = pd.read_sql(query, conn)
 
 
-map = folium.Map(location=[46.879002,-103.789879], zoom_start=5.4, tiles=r'https://server.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', attr="I dont know what to put here lol.", tooltip= "Test")
+### You do not need to this, but since I wanted to make a tooltip that displays the GDP value with the county name, the SQL query needs to be ingested into the JSON file so it can work.  
+rpiJSON = rpiResults[['geofips', 'gdp2022']]
+
+# Load the existing GeoJSON file
+with open(r'C:\Users\nbwan\Python\New folder\data\counties.geojson', 'r') as geojson_file:
+    geojson_data = json.load(geojson_file)
+
+# Create a dictionary to map geofips to GDP values
+geofips_gdp_mapping = dict(zip(rpiJSON['geofips'], rpiJSON['gdp2022']))
+
+# Update the GeoJSON properties with GDP values based on geofips
+for feature in geojson_data['features']:
+    geoid = feature['properties']['GEOID']
+    if geoid in geofips_gdp_mapping:
+        feature['properties']['gdp2022'] = geofips_gdp_mapping[geoid]
+
+# Save the updated GeoJSON
+with open('updated_geojson_file.json', 'w') as updated_geojson_file:
+    json.dump(geojson_data, updated_geojson_file, indent=2)
+
+### Create folium map, folium will write the HTML, Leaflet for you as long as you input and key the data correctly between the two sources.
+map = folium.Map(location=[46.879002,-103.789879], zoom_start=5.4, tiles=r'https://server.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', attr="US GDP 2022 Choropleth map by county.")
 
 folium.Choropleth(
     geo_data = counties,
@@ -35,13 +58,16 @@ folium.Choropleth(
     line_color = 'white'
 ).add_to(map)
 
+SQLJson = os.path.join('data', 'updated_geojson_file.json')
 
+### This is to add a hover effect when the cursor is above a county, it will display its name. I tried to add the GDP result but since that is in a SQL query its a bit hard to add to a JSON file. 
 g = folium.GeoJson(
-    counties,
+    SQLJson,
     name='geojson'
 ).add_to(map)
 
-folium.GeoJsonTooltip(fields=["NAME"]).add_to(g)
+folium.GeoJsonTooltip(fields=["NAME", 'gdp2022'], aliases=["County Name", "GDP 2022"]).add_to(g)
+from folium.plugins import FloatSlider
 
 
-map.save('index.html')
+### Save to index.html and then view it in
